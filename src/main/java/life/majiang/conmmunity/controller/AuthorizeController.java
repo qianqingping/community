59ca714419b22549ceafc957e3355b8819b9ee71
test001 <test001@qq.com>
2020-02-25 21:00:30 +0800

    github authoried

--- src/main/java/life/majiang/conmmunity/controller/AuthorizeController.java
+++ src/main/java/life/majiang/conmmunity/controller/AuthorizeController.java
@@ -1,5 +1,8 @@
 package life.majiang.conmmunity.controller;
 
+import life.majiang.conmmunity.dto.AccessTokenDto;
+import life.majiang.conmmunity.provider.GithubPrivider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,8 +12,19 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubPrivider githubPrivider;
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code,@RequestParam(name="state") String state){
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setClient_id("Iv1.da649b87bf24b198");
+        accessTokenDto.setClient_secret("c9819ef73674d3cba790238714f910fbfca331d4");
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_url("http://localhost:8887/callback");
+        accessTokenDto.setState(state);
+        githubPrivider.getAccessToken(accessTokenDto);
         return"index";
 
     }
