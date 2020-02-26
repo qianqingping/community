7aa24021151ff72d1b9051f9eedae8d30cb608b6
qianqingping <362151510@qq.com>
2020-02-26 15:48:33 +0800

    login comple

--- src/main/java/life/majiang/conmmunity/controller/AuthorizeController.java
+++ src/main/java/life/majiang/conmmunity/controller/AuthorizeController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  */
@@ -29,7 +32,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(secret);
@@ -38,8 +42,15 @@ public class AuthorizeController {
         accessTokenDto.setState(state);
         String accessToken = githubPrivider.getAccessToken(accessTokenDto);
         GithubUser user = githubPrivider.getUer(accessToken);
-        System.out.println(user.getName());
-        return"index";
+        if(user!=null){
+            //登录成功，xiecookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
+
 
     }
 }
