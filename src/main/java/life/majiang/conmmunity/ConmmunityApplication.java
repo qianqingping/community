ad3461ef268e34a5450086697daee387a2632cd4
qianqingping <362151510@qq.com>
2020-02-27 15:24:14 +0800

    connection mysql

--- src/main/java/life/majiang/conmmunity/ConmmunityApplication.java
+++ src/main/java/life/majiang/conmmunity/ConmmunityApplication.java
@@ -1,9 +1,12 @@
 package life.majiang.conmmunity;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("life.majiang.community.mapper")
 public class ConmmunityApplication {
 
     public static void main(String[] args) {
--- src/main/java/life/majiang/conmmunity/controller/AuthorizeController.java
+++ src/main/java/life/majiang/conmmunity/controller/AuthorizeController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
