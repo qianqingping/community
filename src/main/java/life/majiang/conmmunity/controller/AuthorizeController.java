1303168be1bc42e3f36ce96724675e7cdf3bd8fe
test001 <test001@qq.com>
2020-03-04 19:49:02 +0800

    avatar

--- src/main/java/life/majiang/conmmunity/controller/AuthorizeController.java
+++ src/main/java/life/majiang/conmmunity/controller/AuthorizeController.java
@@ -57,6 +57,7 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            user.setAvatarUrl(githubUser.getAvatar_url());
             userMapper.insert(user);
            // request.getSession().setAttribute("user",githubUser);
             reponse.addCookie(new Cookie("token",token));
